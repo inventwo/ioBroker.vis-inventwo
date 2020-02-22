5d426bac685e839958ac6101df4540a3011240c0
jkvarel <jannis@kammann.cc>
2020-02-22 22:01:30 +0000

    Initial commit

--- widgets/inventwo/js/inventwo.js
+++ widgets/inventwo/js/inventwo.js
@@ -61,7 +61,7 @@ vis.binds.inventwo = {
                     vis.setValue(oid,valTrue);
                 }
 
-                $(el).children().html("vars:<br>" + vis.states[oid + '.val'] + "<br>" + vis.states['0_userdata.0.vis-inventwo.background-color'] + "<br> h " + oid);
+                $(el).children().html("vars:<br>" + vis.states[oid + '.val'] + "<br>" + vis.states['0_userdata.0.vis-inventwo.background-color.val'] + "<br> h " + oid);
 
                 
             }).on('touchmove', function () {
