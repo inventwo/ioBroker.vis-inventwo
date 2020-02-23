f77935b6e869eebeb2b1e0a8c1c25860cd172d0b
jkvarel <jannis@kammann.cc>
2020-02-23 15:57:08 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,37 +1,87 @@
-![Logo](img/i_150.png)
-
+![Logo](admin/vis-inventwo.png)
 # ioBroker.vis-inventwo
 
-### Schalter Widgets für den ioBroker.vis Adapter
+[![NPM version](http://img.shields.io/npm/v/iobroker.vis-inventwo.svg)](https://www.npmjs.com/package/iobroker.vis-inventwo)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.vis-inventwo.svg)](https://www.npmjs.com/package/iobroker.vis-inventwo)
+![Number of Installations (latest)](http://iobroker.live/badges/vis-inventwo-installed.svg)
+![Number of Installations (stable)](http://iobroker.live/badges/vis-inventwo-stable.svg)
+[![Dependency Status](https://img.shields.io/david/jkvarel/iobroker.vis-inventwo.svg)](https://david-dm.org/jkvarel/iobroker.vis-inventwo)
+[![Known Vulnerabilities](https://snyk.io/test/github/jkvarel/ioBroker.vis-inventwo/badge.svg)](https://snyk.io/test/github/jkvarel/ioBroker.vis-inventwo)
 
+[![NPM](https://nodei.co/npm/iobroker.vis-inventwo.png?downloads=true)](https://nodei.co/npm/iobroker.vis-inventwo/)
 
-![Beispiel](img/set.png)
+## vis-inventwo adapter for ioBroker
 
----
+Describe your project here
 
-## Unterstützung
+## Developer manual
+This section is intended for the developer. It can be deleted later
 
-Falls Dir unsere Arbeit gefällt und Du uns unterstützen möchtest, wir freuen uns über jede Spende.
+### Getting started
 
-(Dieser Link führt zu unserem PayPal-Konto und steht in keiner Verbindung zum ioBroker)
+You are almost done, only a few steps left:
+1. Create a new repository on GitHub with the name `ioBroker.vis-inventwo`
 
-[![Spende](img/spende.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GQPD3G25CKTEJ&source=url)
+1. Push all files to the GitHub repo. The creator has already set up the local repository for you:  
+	```bash
+	git push origin master
+	```
+1. Head over to [main.js](main.js) and start programming!
 
----
+### Scripts in `package.json`
+Several npm scripts are predefined for your convenience. You can run them using `npm run <scriptname>`
+| Script name | Description                                              |
+|-------------|----------------------------------------------------------|
+| `test:js`   | Executes the tests you defined in `*.test.js` files.     |
+| `test:package`    | Ensures your `package.json` and `io-package.json` are valid. |
+| `test` | Performs a minimal test run on package files and your tests. |
+| `lint` | Runs `ESLint` to check your code for formatting errors and potential bugs. |
 
-## Changelog / Änderungsprotokoll
+### Writing tests
+When done right, testing code is invaluable, because it gives you the 
+confidence to change your code while knowing exactly if and when 
+something breaks. A good read on the topic of test-driven development 
+is https://hackernoon.com/introduction-to-test-driven-development-tdd-61a13bc92d92. 
+Although writing tests before the code might seem strange at first, but it has very 
+clear upsides.
 
-### 0.0.1 (Erstveröffentlichung)
-- Die Idee ist geboren
+The template provides you with basic tests for the adapter startup and package files.
+It is recommended that you add your own tests into the mix.
 
----
+### Publishing the adapter
+Since you have chosen GitHub Actions as your CI service, you can 
+enable automatic releases on npm whenever you push a new git tag that matches the form 
+`v<major>.<minor>.<patch>`. The necessary steps are described in `.github/workflows/test-and-release.yml`.
 
-## Lizens
+To get your adapter released in ioBroker, please refer to the documentation 
+of [ioBroker.repositories](https://github.com/ioBroker/ioBroker.repositories#requirements-for-adapter-to-get-added-to-the-latest-repository).
 
-Urheberrechte (c) 2020 [jkvarel](https://github.com/jkvarel) und [skvarel](https://github.com/skvarel) von [inventwo](https://github.com/inventwo)
+### Test the adapter manually on a local ioBroker installation
+In order to install the adapter locally without publishing, the following steps are recommended:
+1. Create a tarball from your dev directory:  
+	```bash
+	npm pack
+	```
+1. Upload the resulting file to your ioBroker host
+1. Install it locally (The paths are different on Windows):
+	```bash
+	cd /opt/iobroker
+	npm i /path/to/tarball.tgz
+	```
 
+For later updates, the above procedure is not necessary. Just do the following:
+1. Overwrite the changed files in the adapter directory (`/opt/iobroker/node_modules/iobroker.vis-inventwo`)
+1. Execute `iobroker upload vis-inventwo` on the ioBroker host
 
-MIT License (nur in englisch / englisch only)
+## Changelog
+
+### 0.0.1
+* (jkvarel) initial release
+
+## License
+MIT License
+
+Copyright (c) 2020 jkvarel <jkvarel@inventwo.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -50,5 +100,3 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
----
--- main.js
+++ main.js
@@ -1,124 +1,156 @@
-'use strict';
+"use strict";
 
+/*
+ * Created with @iobroker/create-adapter v1.21.1
+ */
 
-const utils = require('@iobroker/adapter-core'); // Get common adapter utils
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
 
-const adapter = utils.Adapter('vis-inventwo');
+// Load your modules here, e.g.:
+// const fs = require("fs");
 
+class visInventwo extends utils.Adapter {
 
-adapter.on('ready', function () {
-    main();
-});
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: "vis-inventwo",
+		});
+		this.on("ready", this.onReady.bind(this));
+		this.on("objectChange", this.onObjectChange.bind(this));
+		this.on("stateChange", this.onStateChange.bind(this));
+		// this.on("message", this.onMessage.bind(this));
+		this.on("unload", this.onUnload.bind(this));
+	}
 
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		// Initialize your adapter here
 
+		// The adapters config (in the instance object everything under the attribute "native") is accessible via
+		// this.config:
+		this.log.info("config option1: " + this.config.option1);
+		this.log.info("config option2: " + this.config.option2);
 
-function main() {
+		/*
+		For every state in the system there has to be also an object of type state
+		Here a simple template for a boolean variable named "testVariable"
+		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+		*/
+		await this.setObjectAsync("testVariable", {
+			type: "state",
+			common: {
+				name: "testVariable",
+				type: "boolean",
+				role: "indicator",
+				read: true,
+				write: true,
+			},
+			native: {},
+		});
 
-    if(adapter.config.Button == "" || !adapter.config.Button){
-        adapter.config.Button="#333333";
-    }
-    if(adapter.config.Active == "" || !adapter.config.Active){
-        adapter.config.Active="#455618";
-    }
-    if(adapter.config.Text == "" || !adapter.config.Text){
-        adapter.config.Text="#C7C7C7";
-    }
-    if(adapter.config.Radius == "" || !adapter.config.Radius){
-        adapter.config.Radius=10;
-    }
-    if(adapter.config.Stripes == "" || !adapter.config.Stripes){
-        adapter.config.Stripes="#393939";
-    }
-    if(adapter.config.Background == "" || !adapter.config.Background){
-        adapter.config.Background='#989898';
-    }
+		// in this template all states changes inside the adapters namespace are subscribed
+		this.subscribeStates("*");
 
-    adapter.setObjectNotExists('CSS', {
-                                type: 'channel',
-                                common: {
-                                    role:  "inventwo",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
+		/*
+		setState examples
+		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+		*/
+		// the variable testVariable is set to true as command (ack=false)
+		await this.setStateAsync("testVariable", true);
 
-    adapter.setObjectNotExists('CSS.Button', {
-                                type: 'state',
-                                common: {
-                                    name:  "Button-Color",
-                                    type:  "string",
-                                    role:  "inventwo.color",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Button', {val: adapter.config.Button, ack: true});
+		// same thing, but the value is flagged "ack"
+		// ack should be always set to true if the value is received from or acknowledged from the target system
+		await this.setStateAsync("testVariable", { val: true, ack: true });
 
-    adapter.setObjectNotExists('CSS.Active', {
-                                type: 'state',
-                                common: {
-                                    name:  "Button-Active-Color",
-                                    type:  "string",
-                                    role:  "inventwo.color",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Active', {val: adapter.config.Active, ack: true});
-   
-    adapter.setObjectNotExists('CSS.Text', {
-                                type: 'state',
-                                common: {
-                                    name:  "Button-Text-Color",
-                                    type:  "string",
-                                    role:  "inventwo.color",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Text', {val: adapter.config.Text, ack: true});
-   
-     adapter.setObjectNotExists('CSS.Radius', {
-                                type: 'state',
-                                common: {
-                                    name:  "Button-Radius",
-                                    type:  "string",
-                                    role:  "inventwo.design",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Radius', {val: adapter.config.Radius, ack: true});       
+		// same thing, but the state is deleted after 30s (getState will return null afterwards)
+		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
-    adapter.setObjectNotExists('CSS.Stripes', {
-                                type: 'state',
-                                common: {
-                                    name:  "Stripes-Active-Color",
-                                    type:  "string",
-                                    role:  "inventwo.color",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Stripes', {val: adapter.config.Stripes, ack: true});    
+		// examples for the checkPassword/checkGroup functions
+		let result = await this.checkPasswordAsync("admin", "iobroker");
+		this.log.info("check user admin pw iobroker: " + result);
 
-    adapter.setObjectNotExists('CSS.Background', {
-                                type: 'state',
-                                common: {
-                                    name:  "Background-Color",
-                                    type:  "string",
-                                    role:  "inventwo.color",
-                                    read:  true,
-                                    write: true
-                                },
-                                native: {}
-                            });
-        adapter.setState('CSS.Background', {val: adapter.config.Background, ack: true});
+		result = await this.checkGroupAsync("admin", "admin");
+		this.log.info("check group user admin group admin: " + result);
+	}
 
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			this.log.info("cleaned everything up...");
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed object changes
+	 * @param {string} id
+	 * @param {ioBroker.Object | null | undefined} obj
+	 */
+	onObjectChange(id, obj) {
+		if (obj) {
+			// The object was changed
+			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+		} else {
+			// The object was deleted
+			this.log.info(`object ${id} deleted`);
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+
+	// /**
+	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+	//  * Using this method requires "common.message" property to be set to true in io-package.json
+	//  * @param {ioBroker.Message} obj
+	//  */
+	// onMessage(obj) {
+	// 	if (typeof obj === "object" && obj.message) {
+	// 		if (obj.command === "send") {
+	// 			// e.g. send email or pushover or whatever
+	// 			this.log.info("send command");
+
+	// 			// Send response in callback if required
+	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+	// 		}
+	// 	}
+	// }
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new visInventwo(options);
+} else {
+	// otherwise start the instance directly
+	new visInventwo();
 }
