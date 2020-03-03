a385f4297f24b843af5cc0c33a54b7366d6175bd
jkvarel <jannis@kammann.cc>
2020-03-03 18:14:19 +0000

    modified:   widgets/vis-inventwo.html 	modified:   widgets/vis-inventwo/js/vis-inventwo.js

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -128,6 +128,13 @@ vis.binds["vis-inventwo"] = {
         /*
     },
 */
+    hexToRgbWithOpacity: function(hex,opacity){
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+        var rgba = result ? ("rgba(" + parseInt(result[1], 16) + "," + parseInt(result[2], 16) + "," + parseInt(result[3], 16) + "," + opacity + ")") : null;
+        return rgba;
+    },
     handleToggle: function (el, data) {
 
             var $this = $(el);
