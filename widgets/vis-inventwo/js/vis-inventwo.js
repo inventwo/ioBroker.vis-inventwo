0d5da721001b63a0a900a2161b7a3c6442f7b625
jkvarel <jannis@kammann.cc>
2020-03-03 16:45:06 +0000

    modified:   widgets/vis-inventwo.html 	modified:   widgets/vis-inventwo/js/vis-inventwo.js

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -142,6 +142,18 @@ vis.binds["vis-inventwo"] = {
     },
     */
 
+    hexToRgbWithOpacity: function(hex,opacity){
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16),
+            a: opacity
+        } : null;
+
+    },
+
     handleToggle: function (el, data) {
 
             var $this = $(el);
