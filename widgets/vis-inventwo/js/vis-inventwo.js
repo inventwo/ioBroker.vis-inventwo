fba498f3aa00848285f6c5ade156013d68a6406a
jkvarel <jannis@kammann.cc>
2020-03-08 15:26:37 +0000

    modified:   widgets/vis-inventwo/js/vis-inventwo.js

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -5,7 +5,7 @@
 */
 "use strict";
 // add translations for edit mode
-$.get( 'adapter/vis-inventwo/words.js', function(script) {
+$.get( 'adapter/vis-inventwo/admin/words.js', function(script) {
     let translation = script.substring(script.indexOf('{'), script.length);
     translation = translation.substring(0, translation.lastIndexOf(';'));
     $.extend(systemDictionary, JSON.parse(translation));
