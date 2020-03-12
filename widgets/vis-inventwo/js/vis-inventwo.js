2450c43bdeae06d3cdb6b9d9a13ac68cac16b218
jkvarel <jannis@kammann.cc>
2020-03-12 16:51:26 +0100

    Range Slider

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -226,8 +226,8 @@ vis.binds["vis-inventwo"] = {
 		$this.css("background",data.iSliderColor);
 		$this.css("border","0px");
 
-		let topPos = (data.iSliderKnobSize - $this.height()) / 2 * (-1);
-		$this.children().css("top",topPos);
+		let topPos = ((data.iSliderKnobSize - $this.height()) / 2) * (-1);
+		$this.children().css("top",topPos + "px");
 		$this.children().css("border","0px");
 
 		vis.states.bind(oid + ".val",function () {
