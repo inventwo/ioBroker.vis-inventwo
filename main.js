c13b1f2f28ea4c121e0b3fa8df01df789035eefd
skvarel <skvarel@inventwo.com>
2020-03-29 12:26:26 +0000

    initial commit

--- main.js
+++ main.js
@@ -227,7 +227,7 @@ class visInventwo extends utils.Adapter {
 			// The state was changed
             this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
             
-            if(state === "CSS.ImgName"){
+            if(state === "vis-inventwo.0.CSS.ImgName"){
                 this.setStateAsync("CSS.Wallpaper", { val: "url('/vis/widgets/vis-inventwo/set/backgrounds/"  + id + "')", ack: true });
             }
 		} else {
