d7ec0d6a5b53015b9302de8a403cda3f80ab6115
jkvarel <jannis@kammann.cc>
2020-04-05 19:09:28 +0200

    Nav fix

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -372,7 +372,7 @@ vis.binds["vis-inventwo"] = {
 		else{
 			if(data.iColCount  !== "" && data.iColCount > 0){
 				let jsondata = vis.states.attr(data.oid + ".val");
-				if(jsondata && (typeof jsondata === 'string') || jsondata instanceof String)
+				if(jsondata && typeof jsondata === 'string')
 					jsondata = JSON.parse(jsondata);
 
 				console.log(jsondata);
