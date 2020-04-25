82aea4b475478a448cfc2b4a112797c3fb0d64e2
jkvarel <jannis@kammann.cc>
2020-04-25 16:27:35 +0200

    Flip Clock

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -515,7 +515,7 @@ vis.binds["vis-inventwo"] = {
 		//
 		$this.find('.vis-inventwo-flip-clock-text').css('color',data.iTextColor);
 		$this.find('.vis-inventwo-flip-clock-text').css('font-size',data.iClockTextSize + "px");
-		$this.find('.vis-inventwo-flip-clock-text').css('line-height',data.height + "px");
+		//$this.find('.vis-inventwo-flip-clock-text').css('line-height',data.height + "px");
 		//
 		$this.find('.vis-inventwo-flip-clock-flip').css('background',data.iBackgroundColor);
 		$this.find('.vis-inventwo-flip-clock-flip-upper').css('background',data.iBackgroundColor);
