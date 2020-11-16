7cb9e3799da0fd9e6bd3796f20604ae7947c36e7
jkvarel <jannis@kammann.cc>
2020-11-16 18:55:52 +0100

    # 2.5.10

--- widgets/vis-inventwo/js/vis-inventwo.js
+++ widgets/vis-inventwo/js/vis-inventwo.js
@@ -2224,29 +2224,32 @@ vis.binds["vis-inventwo"] = {
 	getImgColorFilter: function (color, wid) {
 
 		let filter = "";
+		color = color.toLowerCase();
 
-		vis.conn._socket.emit('getState', "vis-inventwo.0.CSS." + color, function (err, obj) {
-			if(obj != undefined){
-				filter = obj.val;
-			}
-			else{
-				filter = vis.binds["vis-inventwo"].colorFilterGenerator(color);
-				vis.conn._socket.emit("setObject", "vis-inventwo.0.CSS." + color, {
-					type: "state",
-					common: {
-						name: color,
-						type: "string",
-						role: "inventwo.color",
-						read: true,
-						write: true,
-					},
-					native: {},
-				});
+		if(/^#[0-9A-F]{6}$/i.test(color)) {
 
-				vis.setValue("vis-inventwo.0.CSS." + color, filter);
-			}
-			$('#' + wid).find('img').css('filter', filter.substring(8, filter.length -1));
-		});
+			vis.conn._socket.emit('getState', "vis-inventwo.0.intern.ColorFilter." + color.substring(1), function (err, obj) {
+				if (obj != undefined) {
+					filter = obj.val;
+				} else {
+					filter = vis.binds["vis-inventwo"].colorFilterGenerator(color);
+					vis.conn._socket.emit("setObject", "vis-inventwo.0.intern.ColorFilter." + color.substring(1), {
+						type: "state",
+						common: {
+							name: color,
+							type: "string",
+							role: "inventwo.color",
+							read: true,
+							write: true,
+						},
+						native: {},
+					});
+
+					vis.setValue("vis-inventwo.0.intern.ColorFilter." + color.substring(1), filter);
+				}
+				$('#' + wid).find('img').css('filter', filter.substring(8, filter.length - 1));
+			});
+		}
 
 	},
 
