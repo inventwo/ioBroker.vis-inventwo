1a392e869d2966b83690ac9c3eb5374425e3d23f
Jannis <jannis@kammann.cc>
2022-03-29 18:29:01 +0200

    # test

---
+++ widgets/vis-inventwo-old/js/vis-inventwo-old.js
@@ -0,0 +1,14 @@
+/*
+	ioBroker.vis vis-inventwo Widget-Set
+	version: "3.x.x"
+	Copyright 2021 jkvarel jkvarel@inventwo.com
+*/
+"use strict";
+// add translations for edit mode
+
+
+
+vis.binds["vis-inventwo-old"] = {
+
+
+};
