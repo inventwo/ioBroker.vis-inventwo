8f2e1c243dc5b657d4fa952c439ebd90b1d97222
Jannis Kammann <jannis@kammann.cc>
2026-04-24 18:41:39 +0200

    fixes

--- test/unit.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const { tests } = require("@iobroker/testing");
-
-// Run unit tests - See https://github.com/iobroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
